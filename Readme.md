711ab384fa320b6f42b883f61959e4091796b2c8
jlum85 <jlum@wanadoo.fr>
2020-04-16 19:38:17 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Firebase
+
+## ressources
+
+tuto from :
+https://www.freecodecamp.org/news/how-to-build-a-todo-application-using-reactjs-and-firebase/
+
+https://github.com/Sharvin26/TodoApp
+
+Attention : avant le firebase init , il faut faire firebase login
+
+pour tester la route app.get("/todos", getAllTodos); cf API/todo.js
+prendre l'url de la fonction + /todos
+
+https://us-central1-todoapp-c29da.cloudfunctions.net/api/todos
---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+const app = require("express")();
+
+const {
+  getAllTodos,
+  // getOneTodo,
+  postOneTodo,
+  deleteTodo,
+  // editTodo
+} = require("./APIs/todos");
+
+app.get("/todos", getAllTodos);
+//app.get('/todo/:todoId', getOneTodo);
+app.post("/todo", postOneTodo);
+app.delete("/todo/:todoId", deleteTodo);
+//app.put('/todo/:todoId', editTodo);
+
+exports.api = functions.https.onRequest(app);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
