697061ca970dbfa6ce3f769460374b595b7afcc2
jlum85 <jlum@wanadoo.fr>
2020-04-16 20:57:31 +0200

    signup

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,18 @@ const {
   editTodo,
 } = require("./APIs/todos");
 
+const { loginUser, signUpUser } = require("./APIs/users");
+
+// Todos
 app.get("/todos", getAllTodos);
 //app.get('/todo/:todoId', getOneTodo);
 app.post("/todo", postOneTodo);
 app.delete("/todo/:todoId", deleteTodo);
 app.put("/todo/:todoId", editTodo);
+
+// Users
+app.post("/login", loginUser);
+app.post("/signup", signUpUser);
 
 exports.api = functions.https.onRequest(app);
 
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCpOPe_U-72_76-C0yzl67uEgmsK_KEAVI",
+  authDomain: "todoapp-c29da.firebaseapp.com",
+  databaseURL: "https://todoapp-c29da.firebaseio.com",
+  projectId: "todoapp-c29da",
+  storageBucket: "todoapp-c29da.appspot.com",
+  messagingSenderId: "222939562474",
+  appId: "1:222939562474:web:43d26dcd7c130cec887293",
+  measurementId: "G-DDDXT3ESE3",
+};
